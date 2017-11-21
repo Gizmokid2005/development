7e7d80f9c7cd3f1a556e50a9d8848e57b3b580c6
Gizmokid2005 <gizmokid2005@gmail.com>
2017-11-21 12:48:38 -0800

    Cleaned up speedtest log split to properly handle bad/timeout values from the raw file.

--- ruby/speedtestlogsplit.rb
+++ ruby/speedtestlogsplit.rb
@@ -3,7 +3,23 @@ require 'csv'
 f = File.open('speedtest.log', 'r')
 CSV.open('SpeedTestResults.csv', 'w+') do |c|
   c << ["UnixTime","Date","Download","Upload"]
-  f.readlines.each_slice(5) do |l|
-    c << [Time.parse(l[0].strip).to_time.to_i, Time.parse(l[0].strip).strftime('%Y-%m-%d %H:%M:%S'), l[2][10..14], l[3][8..11]]
+  f.readlines.each do |file, w|
+    lines = file.lines
+    lines.each do |line|
+      if line.start_with?('Sun','Mon','Tue','Wed','Thu','Fri','Sat')
+        @csvline = "#{Time.parse(line.strip).to_time.to_i},#{Time.parse(line.strip).strftime('%Y-%m-%d %H:%M:%S')},"
+      elsif line.start_with?('Ping')
+        line.next
+      elsif line.start_with?('Download')
+        @csvline += "#{line[10..14]},"
+      elsif line.start_with?('Upload')
+        @csvline += "#{line[8..11]}"
+      elsif line.start_with?('Cannot')
+        @csvline = ""
+      elsif line.start_with?(" ")
+        c << @csvline.parse_csv unless @csvline.empty?
+        @csvline = ""
+      end
+    end
   end
 end
