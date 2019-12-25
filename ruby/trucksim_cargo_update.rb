40f3883a6072206e5c380ca58a7a8053d34d7044
Gizmokid2005 <gizmokid2005@gmail.com>
2019-12-25 12:41:52 -0800

    Ruby - Cleaned up trucksim modification script.

--- ruby/trucksim_cargo_update.rb
+++ ruby/trucksim_cargo_update.rb
@@ -1,12 +1,25 @@
-  # Find all the files in the current directory
-  Dir.glob("*.sii") do |file|
-    text = File.read(file)
-    oldmass = text.match(/mass: (.+)/)[1]
-    newmass = (text.match(/mass: (.+)/)[1].to_f * 2).to_s
-    replacemass = text.gsub!(oldmass,newmass)
-    oldrewd = text.match(/unit_reward_per_km: (.+)/)[1]
-    newrewd = (text.match(/unit_reward_per_km: (.+)/)[1].to_f * 1.5).to_s
-    replacerewd = replacemass.gsub!(oldrewd,newrewd)
-    File.open(file, "w") {|f| f.puts replacerewd}
-    puts "Filename is #{file}: MASS was changed from #{oldmass} TO #{newmass} || REWARD was changed from #{oldrewd} TO #{newrewd}"
+# Find all the files in the current directory
+Dir.glob("*.sii") do |file|
+  text = File.read(file)
+  if file.match(/dlc_oversize_cargo/)
+    @massmult = 2
+    @rewdmult = 1.5
+  elsif file.match(/dlc_heavy_cargo/)
+    @massmult = 2
+    @rewdmult = 2
+  elsif file.match(/dlc_forest_harvesting/)
+    @massmult = 2
+    @rewdmult = 1.5
+  else
+    @massmult = 2
+    @rewdmult = 1.5
   end
+  oldmass = text.match(/mass: (.+)/)[1]
+  newmass = (text.match(/mass: (.+)/)[1].to_f * @massmult).round(3).to_s
+  replacemass = text.gsub!(oldmass,newmass)
+  oldrewd = text.match(/unit_reward_per_km: (.+)/)[1]
+  newrewd = (text.match(/unit_reward_per_km: (.+)/)[1].to_f * @rewdmult).round(4).to_s
+  replacerewd = replacemass.gsub!(oldrewd,newrewd)
+  File.open(file, "w") {|f| f.puts replacerewd}
+  puts "Filename is #{file}: MASS was changed from #{oldmass} TO #{newmass} || REWARD was changed from #{oldrewd} TO #{newrewd}"
+end
